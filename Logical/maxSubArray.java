9712f0f1a07f7cf7fddbd2a19424511a5bdea2bc
Samee Sheikh <samee.sheikh@sap.com>
2024-05-26 19:35:23 +0530

    Find max and min

--- Logical/maxSubArray.java
+++ Logical/maxSubArray.java
@@ -2,22 +2,45 @@ package Logical;
 
 public class maxSubArray {
     public static void main(String[] args) {
-        // int a[]={-2,1,-3,4,-1,2};
-        int a[]={-1,-2,-3,-4};
+        // int a[]={2,6,8,1,4};
+        // int a[]={-1,-2,-3,-4};
+        int a[]={-2,1,-3,4,-1,2,1,-5,4};
+        int m1=max(a);
+        int m2=min(a);
+        System.out.println(m1+" "+m2);
+    }
+    static int max(int a[]){
         int m=a[0],s=a[0],s1=0;
         for(int i=1;i<a.length;i++){
             s1=s+a[i];
             if(s1>m){
                 m=s1;
-            } 
+            }
             if(s1<a[i]){
                 s=a[i];
-                m=Integer.max(m,s);
+                m=Integer.max(s,m);
             }
-            else {
+            else{
                 s=s1;
             }
         }
-        System.out.println(m);
+        return m;
+    }
+    static int min(int a[]){
+        int m=a[0],s=a[0],s1=0;
+        for(int i=1;i<a.length;i++){
+            s1=s+a[i];
+            if(s1<m){
+                m=s1;
+            }
+            if(s1>a[i]){
+                s=a[i];
+                m=Integer.min(s,m);
+            }
+            else{
+                s=s1;
+            }
+        }
+        return m;
     }
 }
