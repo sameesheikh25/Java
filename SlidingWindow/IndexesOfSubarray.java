88e1730358ca3e0cce70243c347b4967c926946e
Samee Sheikh <samee.sheikh@sap.com>
2024-06-17 14:49:02 +0530

    Optimised technique with map

--- SlidingWindow/IndexesOfSubarray.java
+++ SlidingWindow/IndexesOfSubarray.java
@@ -1,13 +1,14 @@
 package SlidingWindow;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class IndexesOfSubarray {
     public static void main(String[] args) {
         int arr[]={6, -2, -3, 6, -5, 2, -3, -5, 0, 2, -3, 6, -4, 9, 8 };
         int n=15;
-        List<Integer> a=func(arr,n);
+        List<Integer> a=optimisedMethod(arr,n);
         System.out.println(a);
     }
     static ArrayList<Integer> func(int arr[],int n){
@@ -50,5 +51,21 @@ public class IndexesOfSubarray {
         }
         // return f==true?new ArrayList<>(List.of(s1+1,s2+1)):new ArrayList<>(List.of());
         return new ArrayList<>(List.of(s1+1,s2+1));
+    }
+    static ArrayList<Integer> optimisedMethod(int arr[],int n){
+        HashMap<Integer,Integer> m= new HashMap<>();
+        int s=0,i1=0,i2=0;
+        m.put(s,-1);
+        for(int i=0;i<n;i++){
+            s+=arr[i];
+            if(m.containsKey(s)){
+                i1=m.get(s);
+                i2=i;
+                return new ArrayList<>(List.of(i1,i2));
+            } else {
+                m.put(s,i);
+            }
+        }
+        return new ArrayList<>(List.of());
     }
 }
