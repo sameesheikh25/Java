4e529df0d706cdd641251b2a568c95006bb70e00
sameesheikh25 <102856000+sameesheikh25@users.noreply.github.com>
2024-07-03 10:30:49 +0530

    Get numbers as string

--- Logical/RemoveStudents.java
+++ Logical/RemoveStudents.java
@@ -15,7 +15,12 @@ public class RemoveStudents {
         // System.out.println(Collections.max(arr));
         int n1=Collections.binarySearch(Arrays.stream(a).boxed().toList(), 4);
         // System.out.println(-(n1+1));
-        System.out.println(removeStudents(a));
+        // System.out.println(-(n1+1));
+        // System.out.println(removeStudents(a));
+        // int n2 = removeStudents(a,0,-1,n,"",dp,str);
+        // System.out.println(n2);
+        // print(dp);
+        System.out.println(removeStudents(a, n));
         // print(dp);
     }
     static void removeStudents(int a[],ArrayList<Integer> b,int i1,int n,ArrayList<Integer> arr){
@@ -30,6 +35,38 @@ public class RemoveStudents {
         removeStudents(a,b, i1+1, n, arr);
         b.remove(b.size()-1);
         removeStudents(a, b, i1+1, n, arr);
+    }
+    static int removeStudents(int a[],int n){
+        int max=1;
+        int a1[]=new int[n];
+        String ans="";
+        String str[]=new String[n];
+        Arrays.fill(a1,1);
+        Arrays.fill(str,"");
+        for(int i=1;i<n;i++){
+            int k=a1[i];
+            String s=String.valueOf(a[i]);
+            for(int j=0;j<i;j++){
+                if(a[j]<a[i]){
+                    // a1[i]=Integer.max(a1[i],a1[j]+k);
+                    if(a1[j]+k>a1[i]){
+                        str[i]=s+(str[j].length()==0?String.valueOf(a[j]):str[j]);
+                        a1[i]=a1[j]+k;
+                    }
+                }
+            }
+            // max=Integer.max(max,a1[i]);
+            if(a1[i]>max){
+                StringBuffer s1= new StringBuffer(str[i]);
+                ans=s1.reverse().toString();
+                max=a1[i];
+            }
+        }
+        // for(int i=0;i<n;i++){
+        //     System.out.print(str[i]+" ");
+        // }
+        System.out.println(ans);
+        return max;
     }
     static int removeStudents(int a[],int ind,int prev,int n,int dp[][]){
         if(ind==n){
